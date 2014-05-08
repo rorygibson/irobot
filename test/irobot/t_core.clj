1e8a5cae73cef1d09d57f62d664e38a9ab658a0f
Rory Gibson <rory.gibson@brightnorth.co.uk>
2014-05-08 18:55:51 +0100

    Whitespace

--- test/irobot/t_core.clj
+++ test/irobot/t_core.clj
@@ -26,13 +26,16 @@ Disallow: /")
   (fact "Root paths always match"
     (matching-path "/" #{"/"}) => truthy)
 
+  
   (fact "Exact matches always match"
     (matching-path "/foo" #{"/foo"}) => truthy)
 
+  
   (fact "If one of the Paths is a prefix of Path, we match"
     (matching-path "/foobar" #{"/foo"}) => truthy
     (matching-path "/foobar" #{"/fooba"}) => truthy)
 
+  
   (fact "If there is one element in Paths and it's not a prefix, we don't match"
     (matching-path "/foobar" #{"/baz"}) => falsey)
 
